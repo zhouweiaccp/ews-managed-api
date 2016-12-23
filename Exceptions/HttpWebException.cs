b8794700675fac9b1f7230589b9533f0c3262674
sherlock1982 <nick.orekhov@gmail.com>
2016-12-23 14:24:23 +0200

    added HttpWebException

---
+++ Exceptions/HttpWebException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microsoft.Exchange.WebServices.Data
+{
+    public class HttpWebException : InvalidOperationException
+    {
+        public HttpWebException(HttpResponseMessage response, WebExceptionStatus status)
+        {
+            Response = response;
+            Status = status;
+        }
+
+        public HttpWebException(string message, WebExceptionStatus status)
+            : base(message)
+        {
+            Status = status;
+        }
+
+        public HttpWebException(string message, WebExceptionStatus status, Exception innerException)
+            : base(message, innerException)
+        {
+            Status = status;
+        }
+
+        public HttpResponseMessage Response { get; }
+        public WebExceptionStatus Status { get; }
+    }
+}
